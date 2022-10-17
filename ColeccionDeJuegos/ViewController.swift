31f916b17a1725803f62b07a66e1760c6b5486f3
Jorge Sebastian Herrera Jara <jorge.herrera@tecsup.edu.pe>
2022-10-17 15:11:22 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Jorge Herrera Jara on 17/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
